22d086743a53ba92514753167605481414f2db94
jowatson2011 <jowatson@protonmail.com>
2024-08-13 15:31:46 +0100

    Added eventHandler as props for ActionButton

--- src/Components/ActionButton.jsx
+++ src/Components/ActionButton.jsx
@@ -1,6 +1,6 @@
-function ActionButton({ text }) {
+function ActionButton({ text, eventHandler }) {
   return (
-    <button className=" bg-lime-700 text-nowrap px-5 m-2 rounded-full text-amber-100 shadow-block-action-button ease-out hover:-translate-y-px hover:translate-x-px hover:shadow-button-hover transition-all">
+    <button className=" bg-lime-700 text-nowrap px-5 m-2 rounded-full text-amber-100 shadow-block-action-button ease-out hover:-translate-y-px hover:translate-x-px hover:shadow-button-hover transition-all" onClick={eventHandler}>
       {text}
     </button>
   );
