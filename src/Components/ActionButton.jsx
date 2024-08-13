27a19f5bb2b63bd78393dbfebd2becd363a4cc3d
jowatson2011 <jowatson@protonmail.com>
2024-08-13 15:08:43 +0100

    Added reusable button component for initiating events

---
+++ src/Components/ActionButton.jsx
@@ -0,0 +1,9 @@
+function ActionButton({ text }) {
+  return (
+    <button className=" bg-lime-700 text-nowrap px-5 m-2 rounded-full text-amber-100 shadow-block-action-button ease-out hover:-translate-y-px hover:translate-x-px hover:shadow-button-hover transition-all">
+      {text}
+    </button>
+  );
+}
+
+export default ActionButton;
