e2277c8449c462b71338c5ac53f4b8985fabf34c
jowatson2011 <jowatson@protonmail.com>
2024-08-14 08:30:42 +0100

    Updated styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,13 +4,13 @@ import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 function Header() {
   return (
     <header className="my-2 md:mx-10 sm:max-md:sticky sm:max-md:top-0">
-      <nav className="flex justify-end text-[0.75rem] gap-x-5">
-        <p className=" bg-slate-400">Create Account</p>
+      <nav className="flex justify-end text-[0.75rem] gap-x-5 text-slate-700 italic">
+        <p>Create Account</p>
         <Link to="/login">Login</Link>
         <Link to="/my-account">My Account</Link>
       </nav>
       <Link to="/">
-        <h1 className="text-[70px] m-4 italic font-bold text-right hover:not-italic active:text-lime-700">
+        <h1 className="text-[70px] italic font-bold text-right hover:not-italic active:text-lime-700">
           trove
         </h1>
       </Link>
