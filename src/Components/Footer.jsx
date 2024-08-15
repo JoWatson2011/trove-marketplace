da6ea16412de61c3229faba46ee56be87e1243a0
jowatson2011 <jowatson@protonmail.com>
2024-08-15 11:32:20 +0100

    Corrected footer positioning

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,8 +2,8 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import { Divider } from "@mui/material";
 function Footer() {
   return (
-    <div className=" bottom-0 flex align-center mt-20px">
-      <Divider/>
+    <div className="relative bottom-0 flex flex-col align-center mt-[20px] w-full bg-white">
+      <Divider />
       <p className="text-slate-500">
         This website was built by Jo Watson as a portfolio project.{" "}
         <a
