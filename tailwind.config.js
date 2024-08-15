d76918395f31bfc8c4325d867d33a2c339973129
jowatson2011 <jowatson@protonmail.com>
2024-08-15 11:30:53 +0100

    Added custom shadow for ActionButton component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: ["./src/**/*.{html,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "block-action-button": `
+        1px -1px 1px 1px black,
+        2px -2px 1px 2px black,
+        3px -3px 1px 2px black
+        `,
+        "button-hover": `
+        2px -2px 1px 1px black
+        `,
+      }
+    },
   },
   plugins: [],
 };
-
