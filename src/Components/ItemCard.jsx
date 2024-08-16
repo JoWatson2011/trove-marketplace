532518f3dd8d3177f35079b3dfe883531b600002
jowatson2011 <jowatson@protonmail.com>
2024-08-16 11:43:49 +0100

    Styled ItemCard

--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -2,15 +2,28 @@ import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import { Link } from "react-router-dom";
 function ItemCard({ item }) {
   return (
-    <div className=" w-[18rem] h-auto">
-      <Link to={`/items/${item.item_id}`}>
-        <img src={item.image} className="w-[150px] m-10" />
+    <div className="flex flex-col justify-center mx-1 my-1 w-[286px] h-[300px]  p-3">
+      <Link to={`/items/${item.id}`}>
+        <img
+          src={item.image}
+          className="justify-self-center w-[286px] h-[180px] object-contain mb-3 border border-black rounded-xl"
+        />
       </Link>
       <section>
-        <h3>{item.title}</h3>
-        <div className="flex justify-between border border-black">
-          <p>{`£${item.price.toFixed(2)}`}</p>
-          <button>
+        <div className="flex justify-between items-center">
+          <div>
+            <p className="text-lime-800 ">{item.category}</p>
+            <Link to={`/items/${item.id}`}>
+              <h3 className="font-bold">
+                {item.title.length > 50
+                  ? item.title.slice(0, 50) + "..."
+                  : item.title}
+              </h3>
+              <p>{`£${item.price.toFixed(2)}`}</p>
+            </Link>
+          </div>
+
+          <button className="border border-lime-700 bg-lime-700 rounded-full p-1 hover:bg-lime-500 active:bg-lime-600 ml-2">
             <ShoppingBasketIcon />
           </button>
         </div>
