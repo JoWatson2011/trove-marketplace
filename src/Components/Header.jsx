bf893905ccf948906b2ec3dcca187d905bc7e3b8
jowatson2011 <jowatson@protonmail.com>
2024-08-16 09:40:20 +0100

    Moved footer content into header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,31 +1,47 @@
 import { Link } from "react-router-dom";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
 
 function Header() {
   return (
-    <header className="my-2 md:mx-10 sm:max-md:sticky sm:max-md:top-0">
-      <nav className="flex justify-end text-[0.75rem] gap-x-5 text-slate-700 italic">
-        <p>Create Account</p>
-        <Link to="/login">Login</Link>
-        <Link to="/my-account">My Account</Link>
-      </nav>
+    <header className="my-2 md:mr-10 sm:max-md:sticky sm:max-md:top-0">
+      <div className="flex justify-between">
+        <p className="text-slate-500 text-sm">
+          This website was built by Jo Watson as a portfolio project.{" "}
+          <a
+            href="https://github.com/JoWatson2011/trove-marketplace"
+            className="underline hover:text-black"
+          >
+            Source code here
+            <GitHubIcon className="mx-1" />
+          </a>
+        </p>
+        <nav className="flex justify-end text-[0.75rem] gap-x-5 text-slate-700 italic">
+          <p>Create Account</p>
+          <Link to="/login">Login</Link>
+          <Link to="/my-account">My Account</Link>
+        </nav>
+      </div>
       <Link to="/">
         <h1 className="text-[70px] italic font-bold text-right hover:not-italic active:text-lime-700">
           trove
         </h1>
       </Link>
-      <nav className="flex justify-end space-x-10 m-3">
+      <nav className="flex justify-end space-x-5 my-3">
         <Link to="/items">
-          <p className="font-bold italic hover:not-italic hover:text-lime-700 focus:not-italic">
+          <p className="font-bold italic hover:text-lime-700 hover:bg-slate-50 focus:not-italic  border border-gray-600 rounded-full p-2">
             BUY
           </p>
         </Link>
         <Link to="/list-item">
-          <p className="font-bold italic  hover:not-italic hover:text-lime-700 focus:not-italic">
+          <p className="font-bold italic  hover:text-lime-700 hover:bg-slate-50 border border-gray-600 rounded-full p-2">
             SELL
           </p>
         </Link>
-        <ShoppingBasketIcon className="hover:text-lime-700" />
+        <div className="border border-gray-600 hover:bg-slate-50 hover:text-lime-700 rounded-full px-2 py-1">
+          <ShoppingBasketIcon />
+        </div>
       </nav>
     </header>
   );
