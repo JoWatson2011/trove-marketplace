ee020867d67302c7691abc09abb716989343f145
jowatson2011 <jowatson@protonmail.com>
2024-08-16 12:24:07 +0100

    Displayed product categories on home screen using custom tailwind styles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,23 @@
+import { useContext } from "react";
+import { CategoriesContext } from "../context/CategoriesContext";
 function Home() {
-  return <p>Welcome to trove marketplace!</p>;
+  const { categories } = useContext(CategoriesContext);
+
+  return (
+    <main>
+      <h2 className="text-center font-bold italic">SHOP BY CATEGORY</h2>
+      <ul className="flex flex-wrap justify-around">
+        {categories.map((c) => {
+          return (
+            <div class="w-32 h-32 bg-lime-700 rounded-full overflow-visible flex items-center justify-center relative ">
+              <div class="absolute inset-0 bg-stripes rounded-full clip-half-right"></div>
+              <div class="relative z-10 text-white text-center italic font-bold hover:not-italic">{c.toUpperCase()}</div>
+            </div>
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,34 @@ export default {
         "button-hover": `
         2px -2px 1px 1px black
         `,
-      }
+      },
+      backgroundImage: (theme) => ({
+        stripes:
+          "linear-gradient(45deg, transparent 25%, black 25%, black 50%, transparent 50%, transparent 75%, black 75%, black)",
+      }),
+      clipPath: {
+        half: "inset(50% 0% 0 0)",
+        "half-right": "inset(0 0 50% 0%)",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".clip-half": {
+          "clip-path": "inset(0 0 50% 0)",
+        },
+        ".clip-half-right": {
+          "clip-path": "inset(50% 0 0 00)",
+        },
+        ".bg-stripes": {
+          "background-image":
+            "linear-gradient(45deg, transparent 25%, green 25%, green 50%, transparent 50%, transparent 75%, green 75%, green)",
+          "background-size": "20px 20px",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
