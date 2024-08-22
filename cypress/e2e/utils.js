7bead6e17e4b9ae79e3a168baf1e87f39a856376
jowatson2011 <jowatson@protonmail.com>
2024-08-22 08:47:48 +0100

    Returned intercept object from function

--- cypress/e2e/utils.js
+++ cypress/e2e/utils.js
@@ -1,7 +1,6 @@
-exports.mockResponseStatusCode = (path, statusCode, req, body) =>
-  cy
-    .intercept(req, `https://fakestoreapi.com${path}`, {
-      statusCode,
-      body,
-    })
-    .as("submit");
+exports.mockResponseStatusCode = (path, statusCode, req, body) => {
+  return cy.intercept(req, `https://fakestoreapi.com${path}`, {
+    statusCode,
+    body,
+  });
+};
