b860288f868d6a1a1e048bbb3040b884e23b0887
jowatson2011 <jowatson@protonmail.com>
2025-01-22 09:30:42 +0000

    Implement middleware to handle redirects to login on protected routes

--- src/app/list-item/page.tsx
+++ src/app/list-item/page.tsx
@@ -1,21 +1,6 @@
 "use client";
 import ListItem from "../../Components/ListItem";
-import { redirect } from "next/navigation";
-import { getToken } from "../actions";
-import { useEffect, useState } from "react";
 
 export default function Page() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    getToken().then((token) => {
-      setIsLoggedIn(!!token);
-    });
-  }, []);
-
-  if (isLoggedIn) {
-    return <ListItem />;
-  } else {
-    redirect("/login");
-  }
+  return <ListItem />;
 }
--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -1,21 +1,7 @@
 "use client";
-import { useEffect, useState } from "react";
-import { redirect } from "next/navigation";
 import MyAccount from "../../Components/ListItem";
-import { getToken } from "../actions";
 
 export default function Page() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    getToken().then((token) => {
-      setIsLoggedIn(!!token);
-    });
-  }, []);
-
-  if (isLoggedIn) {
     return <MyAccount />;
-  } else {
-    redirect("/login");
-  }
+
 }
