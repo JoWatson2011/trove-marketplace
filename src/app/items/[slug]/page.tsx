8e40a2f9a47038e6d70fcf3d0f37794ea91f3b8f
jowatson2011 <jowatson@protonmail.com>
2025-02-04 10:07:45 +0000

    Fix: Used generateStaticParams for /items/[slug]

--- src/app/items/[slug]/page.tsx
+++ src/app/items/[slug]/page.tsx
@@ -1,5 +1,21 @@
 import Item from "../../../Components/Item";
+import { getRequest } from "../../../utils/api";
 
+export async function generateStaticParams() {
+  const items = await getRequest("/products");
+
+  return items.map(
+    (item: {
+      id: number;
+      image: string;
+      title: string;
+      price: number;
+      description: string;
+    }) => ({
+      slug: `${item.id}`,
+    })
+  );
+}
 export default function Page() {
   return <Item />;
 }
